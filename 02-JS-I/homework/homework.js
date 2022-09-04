656100c247347b0bb7e101c5006e8e1abb4d61ae
alejandro <alejandromarinalvarino@gmail.com>
2022-09-03 19:20:06 -0500

    Ejercicio resuelto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 12;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -35,24 +35,37 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+
+  const resultado = x + y ;
+
+  return resultado;
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  const resultado = x - y ;
+
+  return resultado;
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  const resultado = x * y ;
+
+  return resultado;
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  const resultado = x / y ;
+
+  return resultado;
   
 }
 
@@ -60,6 +73,17 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resultado=false;
+  if(x==y)
+  {
+    resultado= true;
+
+  }else{
+
+    resultado=false;
+  }
+
+  return resultado;
   
 }
 
@@ -67,6 +91,22 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resultado= false;
+
+  if(str1.length==str2.length)
+  {
+
+    resultado=true;
+
+  }else{
+
+    resultado=false;
+
+  }
+
+  return resultado;
+
+
   
 }
 
@@ -74,6 +114,17 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resul=false;
+
+  if( num < 90 )
+  {
+    resul=true;
+  }else{
+
+    resul=false;
+  }
+
+  return resul;
   
 }
 
@@ -81,12 +132,26 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resul=false;
+
+  if( num > 50 )
+  {
+    resul=true;
+  }else{
+
+    resul=false;
+  }
+
+  return resul;
   
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  const resultado = x % y ;
+
+  return resultado;
   
 }
 
@@ -94,6 +159,17 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resul= false;
+
+  if(num % 2 == 0){
+
+    resul= true;
+  }else{
+
+     resul=false;
+  }
+
+  return resul;
   
 }
 
@@ -101,6 +177,17 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  var resul= false;
+
+  if(num % 2 != 0){
+
+    resul= true;
+  }else{
+
+     resul=false;
+  }
+
+  return resul;
   
 }
 
@@ -108,12 +195,20 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  const resultado=(Math.pow(num,2));
+
+  return resultado;
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  const resultado=(Math.pow(num,3));
+
+  return resultado;
+  
+
   
 }
 
@@ -121,11 +216,19 @@ function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
   
+   const resultado = (Math.pow(num,exponent));
+
+   return resultado;
+  
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  
+  num = Math.round(num);
+
+  return num;
   
 }
 
